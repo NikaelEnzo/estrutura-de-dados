debd22a3364352bff4b76bce03f7a8ae4ff6538c
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-29 14:32:33 -0400

    Estrutura de Dados

---
+++ Estrutura-de-Dados.js
@@ -0,0 +1,21 @@
+/*
+Vetores (Array)
+Array estrutura de dados que permite armazenar uma coleção de elementos sob um único nome, uma variável, onde cada elemento e acessado por meio de um índice único
+Armazenados contiguo na memória
+
+push | pop | shift | unshify | splice | concat | slice | indexOf | lastIndexOf | forEach | map | filter | every | soma | find | findindex | reduce
+
+
+Objetos (Object)
+
+listas ligadas
+pilhas (stacks)
+filas (Queue)
+Arvores
+Grafos (Graphs)
+Tabelas de Hash (Hash Tables)
+Conjuntos (sets)
+Mapas (maps)
+Fila de prioridade (priority Queue)
+
+*/
