007a47e120456e92a8cc75a4a67e2379a595d26e
NikaelEnzo <enzo.marthins@gmail.com>
2024-05-08 15:29:32 -0400

    Listas ligadas adicionadas

---
+++ listas-ligadas/Linked-List.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+/*
+
+*/
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+    // Método para adicionar nó
+    prepend(data) {
+        const newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+}
+
+const list = new LinkedList();
+list.prepend(1);
+list.prepend(2);
+list.prepend(3);
+console.log(list)
+/*
+
+
+
+*/
