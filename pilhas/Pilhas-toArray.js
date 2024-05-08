02146e4546a8b704644e44f3c076234ac67099b2
NikaelEnzo <enzo.marthins@gmail.com>
2024-05-08 16:51:07 -0400

    Pilhas toArray adicionada

---
+++ pilhas/Pilhas-toArray.js
@@ -0,0 +1,9 @@
+    // metodo toArray 
+
+Array.prototype.toArray = function() {
+    return this.slice();
+};
+
+let pilha = [10, 20, 30];
+let array = pilha.toArray()
+console.log(array); // saida: [10, 20, 30]  // array
