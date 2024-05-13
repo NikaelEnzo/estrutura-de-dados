b6fbc6d8d1f8e43575c1354f828bbca2fcebae55
NikaelEnzo <enzo.marthins@gmail.com>
2024-05-13 17:06:09 -0400

    Mapas adicionado

---
+++ mapas/maps-set.js
@@ -0,0 +1,14 @@
+let mapa = new Map()
+
+mapa.set('chave1', 'valor1')
+mapa.set('chave2', 'valor2')
+mapa.set('chave3', 'valor3')
+
+console.log(mapa)
+    /*
+Map(3) {
+'chave1' => 'valor1',
+'chave2' => 'valor2',
+'chave3' => 'valor3'
+}
+*/
