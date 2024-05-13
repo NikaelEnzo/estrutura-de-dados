7e5c0a9f601b9eaa05a7a03f4e158fd11109551b
NikaelEnzo <enzo.marthins@gmail.com>
2024-05-13 16:30:00 -0400

    Conjuntos adicionados

---
+++ conjuntos/conjunto-add.js
@@ -0,0 +1,8 @@
+// somente elemntos unicos e funciona no "estilo" objeto
+let conjunto = new Set();
+conjunto.add(1);
+conjunto.add(2);
+conjunto.add(3);
+conjunto.add(4);
+
+console.log(conjunto); // saida Set(3) { 1, 2, 3 }
